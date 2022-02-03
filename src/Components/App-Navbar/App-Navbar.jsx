0ba80bb1c09d3dfd8ce7746522c83dc0ffc56550
WIeZII <83194514+WIeZII@users.noreply.github.com>
2022-02-03 15:59:06 +0100

    coucou

--- src/Components/App-Navbar/App-Navbar.jsx
+++ src/Components/App-Navbar/App-Navbar.jsx
@@ -3,9 +3,9 @@ import { Navbar, Container, Nav,NavDropdown,FormControl,Button,Form} from 'react
 
 export default function AppNavbar() {
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar bg="info" expand="lg">
   <Container fluid>
-    <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+    <Navbar.Brand href="#">Carnet de notes</Navbar.Brand>
     <Navbar.Toggle aria-controls="navbarScroll" />
     <Navbar.Collapse id="navbarScroll">
       <Nav
@@ -14,27 +14,26 @@ export default function AppNavbar() {
         navbarScroll
       >
         <Nav.Link href="#action1">Acceuil</Nav.Link>
-        <Nav.Link href="#action2">Link</Nav.Link>
-        <NavDropdown title="Link" id="navbarScrollingDropdown">
-          <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-          <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
+        <NavDropdown title="Favoris" id="navbarScrollingDropdown favoris">
+          <NavDropdown.Item href="#action3">HTML</NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item href="#action4">CSS</NavDropdown.Item>
           <NavDropdown.Divider />
           <NavDropdown.Item href="#action5">
-            Something else here
+            Js
           </NavDropdown.Item>
         </NavDropdown>
-        <Nav.Link href="#" disabled>
-          Link
-        </Nav.Link>
+        <Nav.Link href="#action2">Statistiques</Nav.Link>
+        <Nav.Link href="#action3">Configuration</Nav.Link>
       </Nav>
       <Form className="d-flex">
         <FormControl
           type="search"
-          placeholder="Search"
+          placeholder="recherche"
           className="me-2"
           aria-label="Search"
         />
-        <Button variant="outline-success">Search</Button>
+        <Button variant="outline-success">Rechercher</Button>
       </Form>
     </Navbar.Collapse>
   </Container>
