9f11add26e5d932e8f242a50a007364847875519
notsaltypepper <88979389+notsaltypepper@users.noreply.github.com>
2022-02-04 11:35:38 +0100

    ajout d'accordeons accueil

--- src/Components/AcceuilPage/AcceuilPage.jsx
+++ src/Components/AcceuilPage/AcceuilPage.jsx
@@ -1,7 +1,39 @@
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom'
+import {
+  Accordion,
+  Tab,
+  Tabs,
+  Card,
+  Placeholder,
+  Button,
+} from 'react-bootstrap'
 
-export default function AcceuilPage(){
-    return(
-        <header>Hello world</header>
-    )
+export default function AcceuilPage() {
+  return (
+    <Accordion defaultActiveKey="0">
+      <Accordion.Item eventKey="0">
+        <Accordion.Header>Statistiques</Accordion.Header>
+        <Accordion.Body>
+          - Total carnets de notes: <br></br>- Total notes: <br></br>- Notes par
+          catégories:
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="1">
+        <Accordion.Header>Carnets de notes</Accordion.Header>
+        <Accordion.Body>
+          <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+        </Accordion.Body>
+      </Accordion.Item>
+    </Accordion>
+  )
 }
--- src/index.js
+++ src/index.js
@@ -1,45 +1,34 @@
-import React,{useState} from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import AppNavbar from './Components/App-Navbar/App-Navbar';
-import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Form from './Components/Carnet-Note/Form';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import CarnetNoteList from './Components/Carnet-Note/CarnetNoteList';
-import AcceuilPage from './Components/AcceuilPage/AcceuilPage';
+import * as React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import AppNavbar from './Components/App-Navbar/App-Navbar'
+import reportWebVitals from './reportWebVitals'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Form from './Components/Carnet-Note/Form'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import CarnetNoteList from './Components/Carnet-Note/CarnetNoteList'
+import AcceuilPage from './Components/AcceuilPage/AcceuilPage'
 import NoteAppPage from './Components/NoteAddPage/NoteAddPage'
-import NoteAddPage from './Components/NoteAddPage/NoteAddPage';
+import NoteAddPage from './Components/NoteAddPage/NoteAddPage'
 
 ReactDOM.render(
-  
   <React.StrictMode>
     <main>
-      
       <Router>
         <header className="mb-5">
           <AppNavbar></AppNavbar>
-          
         </header>
-      
-        
+
         <Routes>
-          
           <Route path="/Acceuil" element={<AcceuilPage />} />
           <Route path="/App" element={<App />} />
           <Route path="/NoteAddPage" element={<NoteAddPage />} />
-          
-          
-          
-     
         </Routes>
       </Router>
     </main>
   </React.StrictMode>,
   document.getElementById('root')
-  );
- 
-
+)
 
 reportWebVitals()
